38cea890d66ff801c1a0597d81787af3edd52246
Bjørn Remseth <la3lma@gmail.com>
2019-05-16 14:00:06 +0200

    Adding checks for error conditions

--- src/deploy-secrets-to-simmgr-test.sh
+++ src/deploy-secrets-to-simmgr-test.sh
@@ -5,30 +5,42 @@ DEPLOYMENT_NAME="$0"
 echo "Preparing deployment named: $DEPLOYMENT_NAME"
 
 if [[ -z "$DEPLOYMENT_SECRETS_HOME]] ; then
-   echo "Error:  SECRETS_DIR not set, cannot progress"
+   echo "Error:  SECRETS_DIR not set, cannot progress" >&2
    exit 1
 fi
 
 if [[ -z $(which kubectl) ]]; then
-   echo "Error: Kubectl not installed.  Please reinstall before attempting again"
+   echo "Error: Kubectl not installed.  Please reinstall before attempting again" >&2
    exit 1
 fi
 
 DEPLOYMENT_PARAMETER_FILE="${DEPLOYMENT_SECRETS_HOME}/${DEPLOYMENT_NAME}.sh"
 
 if [[ ! -f "$DEPLOYMENT_PARAMETER_FILE" ]] ; then
-   echo "Could not file deployment parameter file $DEPLOYMENT_PARAMETER_FILE"
+   echo "Could not file deployment parameter file $DEPLOYMENT_PARAMETER_FILE"    >&2
    exit 1
 fi
 
 # Source the parameter file
 . $DEPLOYMENT_PARAMETER_FILE
 
+if [[ -z "$DEPLOYMENT_CLUSTER_NAME" ]] ; then
+   echo "ERROR: DEPLOYMENT_CLUSTER_NAME variable not set" >&2
+   exit 1
+fi
 
-# Do a bunch of sanity checks based on the parameters from the deployment
-# parameter file
+# Are we in the right cluster?
+if [[ -z $(kubectl config view -o jsonpath='{.contexts[?(@.name == "$DEPLOYMENT_CLUSTER_NAME")].name}') ]] ; then
+   echo "Target cluster '$DEPLOYMENT_CLUSTER_NAME' is unknown." >&2
+   echo 1
+fi
 
-   # XXX TBD
+kubectl config use-context "$DEPLOYMENT_CLUSTER_NAME"
+if [[ "$DEPLOYMENT_CLUSTER_NAME" != "$(kubectl config current-context)"  ]] ; then
+   echo "Could not connect to target cluster $DEPLOYMENT_CLUSTER_NAME"
+   echo 1
+fi
+
 
 # Then do the kubernetes thing
 kubectl create secret generic simmgr-test-secrets \
@@ -42,4 +54,11 @@ kubectl create secret generic simmgr-test-secrets \
          --from-literal functionRequesterIdentifier=${ES2_PLUS_FUNCTION_REQUEST_IDENTIFIER} \
          --from-file idemiaClientCert="${IDEMIA_ES2_JKS}"
 
-echo "Deployment done."
+if 
+
+if [[ $? -eq 0 ]] ; then
+   echo "Deployment to kubernetes cluster did not go well. Please  investigate" >&2
+   exit 1
+fi
+   
+echo "Successul deployment."

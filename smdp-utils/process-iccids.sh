cef480636043bb402d4cf6dba4662e4801a0fd0a
Bjørn Remseth <la3lma@gmail.com>
2019-06-28 06:19:12 +0200

    Fix bug

--- smdp-utils/process-iccids.sh
+++ smdp-utils/process-iccids.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# First and last of the ICCID file that will be processed
+# First and last of the ICCID file that will be processed. 
+# Program is idempotent with respect to output.  Will output CSV file with iccid, confirmation code
 first=1
 last=3
 
@@ -14,6 +15,9 @@ if [[ ! -f "$input" ]] ; then
     exit 1
 fi 
 
+
+echo "ICCID, CONFIRMATION_CODE"
+
 while IFS= read -r iccid
 do
 
@@ -23,7 +27,7 @@ do
 	  if [[ "AVAILABLE" == "$status" ]] ; then
 	      ./update-profile.sh iccid $iccid es2download es2confirm es2profileStatus | awk -F: '{print $2}' | awk -F, '{print $1, $3}'
 	  else
-	      ./update-profile.sh iccid $iccid es2download |  es2profileStatus | awk -F: '{print $2}' | awk -F, '{print $1, $3}'
+	      ./update-profile.sh iccid $iccid es2profileStatus |  grep es2profileStatus | awk -F: '{print $2}' | awk -F, '{print $1, $3}'
 	  fi
       fi
   fi

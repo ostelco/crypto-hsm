c1b5abcbae9ee445c7c4e8403ba98e820f948bf1
Bjørn Remseth <la3lma@gmail.com>
2019-07-02 10:18:46 +0200

    Fix bug, change assumptions about profiles to change.  In particular, we don't want to activate profiles that have been disabled for whatever reason

--- smdp-utils/generate-sql-for-activation-code-updates.sh
+++ smdp-utils/generate-sql-for-activation-code-updates.sh
@@ -16,5 +16,5 @@ grep -v ICCID "$input" | while IFS= read -r iccidMatchingIdLine
 do
   iccid=$(echo $iccidMatchingIdLine | awk '{print $1}')
   matchingid=$(echo $iccidMatchingIdLine | awk 'BEGIN{RS="\r\n"}{print $2}')
-  echo "UPDATE sim_entries SET matchingid = '$matchingid', smdpplusstate = 'RELEASED', provisionstate = 'AVAILABLE' WHERE iccid = '$iccid' and smdpplusstate = 'AVAILABLE';"
+  echo "UPDATE sim_entries SET matchingid = '$matchingid', smdpplusstate = 'RELEASED' WHERE iccid = '$iccid' AND smdpplusstate = 'AVAILABLE'  AND provisionstate = 'AVAILABLE';"
 done
